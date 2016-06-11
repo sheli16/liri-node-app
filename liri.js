60bdec59d8a236d933049767bd40a3f32aa658e5
sheli16 <sheli16@live.seminolestate.edu>
2016-06-11 08:53:01 -0400

    package.JSON, main funcitons, debugging syntax error

--- liri.js
+++ liri.js
@@ -1,17 +1,19 @@
-console.log('are you there?');
+		console.log('are you there?');
 var Twitter = require('twitter');
 var request = require('request');
-
-
+var spotify = require('spotfy')
+ 
+var fs = require('fs');
 var twitters = require('./keys.js');
-
+var textfile = require('./random.txt');
+fs.readfile
 // var info= twitters(JSON.stringify);
-console.log(twitters);
+		console.log(twitters);
 
 var command = process.argv[2];
 var userinput = process.argv[3];
 
-console.log(userinput);
+		console.log(userinput);
 
 switch(command){
 	case 'my-tweets':
@@ -26,48 +28,68 @@ switch(command){
 	 	movie();
 	break;
 
-	
 	case 'do-what-it-says':
 	 	dosay();
 	break;
 }
 
+
+// tweets function 
 function tweets(){
-console.log('getting closer');
+		console.log('tweets function');
+
 // var Twitter = require('twitter');
 var client = new Twitter({
+  
+  // for testing the key information  to be retrieved through the Key.js file
+  consumer_key: 'FOqJxGOD4OEI4YnL21mUekm27',
+  consumer_secret: 'jKPJZPvHvilFVY9eeSdIhlsJzFVz3iTwBPQkH9UL1qu6p25opT',
+  access_token_key: '2344105260-8RbfXTvPqjevuNZUhMeykiEnpW6mxwUAwvdb2bo',
+  access_token_secret: 'Mlo3CiliQadnclXNl0wY6MCHhPf9QZNBi2VdsJtg2utZL',
   // consumer_key: process.env.TWITTER_CONSUMER_KEY,
   // consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   // access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   // access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-
-console.log('pass client');
+		// debugging program marker
+		console.log('pass client'); 
 
 var params = {screen_name: 'nodejs'};
 client.get('.statuses/user_timeline', params, function(error, tweets, response){
   if (!error) throw error;
-  
+  	
+  	// checking for response from twitter
   	console.log(tweets);
     console.log(response);
   });
 };
 
-function spotify(){
-console.log('getting closer');
 
-spotify.search({ type: 'artist','track','track link','album', query: userinput }, function(err, data) {
+// Spotify function 
+function spotify(){
+		console.log('spotify function');
+
+spotify.search({ type: 'artist', spotify: "https://open.spotify.com/artist/08td7MxkoHQkXnWAYD8d6Q", 
+	name: userinput, uri: "spotify:artist:08td7MxkoHQkXnWAYD8d6Q", 
+	query: userinput }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
+ });
+// fs.readFile('random.txt', 'utf8', function(err, data){
+// if (err) {
+// 	return console.log(err);
+// }
+// console.log(data);
+// })
+};
  
-    // Do something with 'data' 
-});
 
+// Movie function 
 function movie(){
-console.log('getting closer');
-var url = 'http://www.omdbapi.com/?t='+ userinput+ '&y=&plot=short&r=json';
+		console.log('movie function');
+var url = 'http://www.omdbapi.com/?t='+ userinput + '&y=&plot=short&r=json';
 
 request(url, function(err, response, body){
 	body = JSON.parse(body); 
@@ -79,10 +101,9 @@ request(url, function(err, response, body){
 	console.log(body.Actors);
 	console.log(body.imdbRating);
 	console.log(body.Poster);
-})
+
  if ( err ) {
         console.log('Error occurred: ' + err);
         return;
-
-
-
+    }
+});
