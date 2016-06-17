44b2ad9ee54fa6f86e95bc575d498fc629c0e4b5
sheli16 <sheli16@live.seminolestate.edu>
2016-06-17 11:04:00 -0400

    delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'FOqJxGOD4OEI4YnL21mUekm27',
-  consumer_secret: 'jKPJZPvHvilFVY9eeSdIhlsJzFVz3iTwBPQkH9UL1qu6p25opT',
-  access_token_key: '2344105260-8RbfXTvPqjevuNZUhMeykiEnpW6mxwUAwvdb2bo',
-  access_token_secret: 'Mlo3CiliQadnclXNl0wY6MCHhPf9QZNBi2VdsJtg2utZL',
-}
